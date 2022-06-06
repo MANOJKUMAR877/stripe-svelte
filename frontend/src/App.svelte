08e6d20637a410f5406b2248bfc88d719aa1931c
manojkumar@877 <kbmanojkumar.20cs@srit.org>
2022-06-06 17:59:37 +0200

    post JWT auth done

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,26 @@
   import "carbon-components-svelte/css/all.css";
   import Form_OP from "./Components/Form_OP.svelte";
   import dataFetch from "./API/fetch";
+  let token = "";
+  import FormOp from "./Components/Form_OP.svelte";
+  import { onMount } from "svelte";
+  onMount(() => {
+    token = window.localStorage.getItem("token");
+    //console.log(token.length > 0)
+  });
 </script>
 
+<hr />
+
+{#if !token.length > 1}
+  <FormOp />
+{/if}
+
+<hr />
+<br />
+<hr />
 <ListRender />
+<hr />
 
 <style>
 </style>
--- frontend/src/Components/Form_OP.svelte
+++ frontend/src/Components/Form_OP.svelte
@@ -1,26 +1,43 @@
 <script>
   import { Form, Button, TextInput, TextArea } from "carbon-components-svelte";
+
   export const data = {
-    title: "",
-    rating: "",
-    body: "",
+    identifier: "",
+    password: "",
+  };
+
+  const submit = async () => {
+    console.log(
+      "val",
+      JSON.stringify({ identifier: data.identifier, password: data.password })
+    );
+    const res = await fetch("http://localhost:1337/api/auth/local", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (res.ok) {
+
+      let d  = await(res.json())
+      console.log(d.jwt)
+      window.localStorage.setItem('token',d.jwt)
+    }
   };
 </script>
 
 <Form
   on:submit={(e) => {
     e.preventDefault();
-    console.log("submit", data);
+    submit();
   }}
 >
-  <label for="title">Enter the Title</label>
-  <TextInput name="title" bind:value={data.title} />
-  <label for="rating">Enter the Rating</label>
-  <TextInput name="rating" type="number" bind:value={data.rating} />
-  <TextArea
-    labelText="Body"
-    placeholder="Enter a description..."
-    bind:value={data.body}
-  />
+  <label for="identifier">Enter the User Name</label>
+  <TextInput name="identifier" bind:value={data.identifier} />
+  <label for="password">Enter the password</label>
+  <TextInput name="password" type="password" bind:value={data.password} />
+
   <Button type="submit">Submit</Button>
 </Form>
