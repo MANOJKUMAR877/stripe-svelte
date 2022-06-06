3d78167e97ac855c7bef6bc78512c461292f6fdc
manojkumar@877 <kbmanojkumar.20cs@srit.org>
2022-06-06 18:45:20 +0200

    POST value have to done

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,10 +6,30 @@
   let token = "";
   import FormOp from "./Components/Form_OP.svelte";
   import { onMount } from "svelte";
+  import AddDetail from "./Components/AddDetail.svelte";
   onMount(() => {
     token = window.localStorage.getItem("token");
     //console.log(token.length > 0)
   });
+  const add = () => {
+    let d = {
+      title: "gf",
+      rating: 6,
+      body: "ESSENCE OF BELLYDANCE ™\nDie ESSENCE OF BELLYDANCE ™ Methode verbindet die Schönheit des (Bauch-)Tanzes mit einer klar strukturierten und erklärten Technik. Dadurch wird es jeder Frau ermöglicht, diesen Tanz zu begreifen und Schritt für Schritt für sich zu erobern.",
+    };
+    fetch("http://localhost:1337/api/reviews/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+
+        Accept: "application/json",
+
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(d),
+    });
+  };
 </script>
 
 <hr />
@@ -17,9 +37,14 @@
 {#if !token.length > 1}
   <FormOp />
 {/if}
-
+<AddDetail />
 <hr />
 <br />
+<button
+  on:click={() => {
+    add();
+  }}>Add</button
+>
 <hr />
 <ListRender />
 <hr />
