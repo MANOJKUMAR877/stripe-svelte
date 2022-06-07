679b58bea1b68b92db8b3171bff7fd22706b0896
manojkumar@877 <kbmanojkumar.20cs@srit.org>
2022-06-07 11:16:55 +0200

    post method done now work with flowbite and delete method

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,51 +3,30 @@
   import "carbon-components-svelte/css/all.css";
   import Form_OP from "./Components/Form_OP.svelte";
   import dataFetch from "./API/fetch";
-  let token = "";
+  export let token = "";
   import FormOp from "./Components/Form_OP.svelte";
   import { onMount } from "svelte";
   import AddDetail from "./Components/AddDetail.svelte";
+import ContentForm from "./Components/ContentForm.svelte";
   onMount(() => {
     token = window.localStorage.getItem("token");
-    //console.log(token.length > 0)
+    console.log(token);
   });
-  const add = () => {
-    let d = {
-      title: "gf",
-      rating: 6,
-      body: "ESSENCE OF BELLYDANCE ™\nDie ESSENCE OF BELLYDANCE ™ Methode verbindet die Schönheit des (Bauch-)Tanzes mit einer klar strukturierten und erklärten Technik. Dadurch wird es jeder Frau ermöglicht, diesen Tanz zu begreifen und Schritt für Schritt für sich zu erobern.",
-    };
-    fetch("http://localhost:1337/api/reviews/", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
 
-        Accept: "application/json",
-
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(d),
-    });
-  };
 </script>
 
 <hr />
 
-{#if !token.length > 1}
-  <FormOp />
-{/if}
+<FormOp />
+
 <AddDetail />
 <hr />
 <br />
-<button
-  on:click={() => {
-    add();
-  }}>Add</button
->
+
 <hr />
 <ListRender />
 <hr />
-
+<br/>
+<ContentForm token={token}/>
 <style>
 </style>
---
+++ frontend/src/Components/ContentForm.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let token;
+  let data = {
+    title: "",
+    rating: "",
+    body: "",
+  };
+  const submit = () => {
+    console.log("fhgjk");
+    fetch("http://localhost:1337/api/reviews/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ data: data }),
+    });
+  };
+</script>
+
+<form
+  on:submit|preventDefault={() => {
+    submit();
+  }}
+>
+  <input bind:value={data.title} />
+  <input bind:value={data.rating} />
+  <textarea bind:value={data.body} />
+  <button on:click>Submit</button>
+</form>
